304e19431303315a9ba6a73ec8d5a578eb93f6a9
ChaitanyaAnnamreddy <achaitanya0505@gmail.com>
2025-06-19 15:02:15 +0530

    fix

--- src/workers/audioProcessor.js
+++ src/workers/audioProcessor.js
@@ -1,7 +1,7 @@
 self.onmessage = async (e) => {
   const { action, data } = e.data;
   if (action === "mixAudio") {
-    // Placeholder: Return first sound (implement actual mixing with Web Audio API or ffmpeg.wasm)
+   
     self.postMessage({ status: "success", action, data: data.sound1 });
   }
 };
